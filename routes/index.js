92304b28058dc5d2516f7c32e26292f754211a98
yaoyongjia <631892354@qq.com>
2021-12-30 10:54:07 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/yyj', function (req, res, next) {
+  const data = {
+    name: 'boyuan',
+    say: 'Hell2o222211112222'
+  }
+  res.json(data)
+})
+
+module.exports = router;
